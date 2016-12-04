26263ea9f734d137686150217402620a1d042e0f
LinKuanLei <103051089@live.asia.edu.tw>
2016-12-05 03:09:59 +0800

    Update and rename hw02.java to hw02_103051089.java

--- src/hw/hw02_103051089.java
+++ src/hw/hw02_103051089.java
@@ -4,9 +4,10 @@ package hw;
 數列一:1 2 4 7 11 16 22 29…
 數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 103051089 林冠磊
  */
-public class hw02 {
+import java.util.Scanner;
+public class hw02_103051089 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
